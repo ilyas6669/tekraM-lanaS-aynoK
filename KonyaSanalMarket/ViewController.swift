fc38cef376db450a744bfbb6ce3220c1b0b5cca9
İlyas Abiyev <999.ilyas.6666@gmail.com>
2020-06-19 15:16:27 +0400

    Initial Commit

---
+++ KonyaSanalMarket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KonyaSanalMarket
+//
+//  Created by İlyas Abiyev on 6/19/20.
+//  Copyright © 2020 İlyas Abiyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
